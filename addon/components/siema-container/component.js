9a57b0e8e4bd0575a925bbc5964743873f410b0b
Ryan Quinn <ryan@mazondo.com>
2018-01-28 13:54:55 -0600

    small fixes

--- addon/components/siema-container/component.js
+++ addon/components/siema-container/component.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import layout from './template';
 import Siema from 'siema';
 import { assign }  from '@ember/polyfills';
+import { scheduleOnce }  from '@ember/runloop';
 
 export default Component.extend({
   layout,
@@ -24,10 +25,12 @@ export default Component.extend({
   },
 
   didInsertElement() {
-    let slidesEl = document.getElementById(`slides-${this.elementId}`);
-    let options = assign({}, this.get('options'), {selector: slidesEl});
-    let siema = new Siema(options);
-    this.set('_siema', siema);
+    scheduleOnce('afterRender', this, () => {
+      let slidesEl = document.getElementById(`slides-${this.elementId}`);
+      let options = assign({}, this.get('options'), {selector: slidesEl});
+      let siema = new Siema(options);
+      this.set('_siema', siema);
+    });
   },
 
   willDestroyElement() {
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 'use strict';
+const path = require('path');
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
-  name: 'ember-cli-siema'
+  name: 'ember-cli-siema',
+  included() {
+    this.import('vendor/siema/siema.min.js');
+    this.import('vendor/shims/siema.js');
+  },
+
+  treeForVendor(vendorTree) {
+    let siemaPath = path.dirname(require.resolve('siema'));
+    let newTree = new Funnel(siemaPath, {
+      files: ['siema.min.js'],
+      destDir: '/siema',
+    });
+
+    return new mergeTrees([vendorTree, newTree]);
+  },
 };
