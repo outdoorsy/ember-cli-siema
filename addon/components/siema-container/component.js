78457e006673c6c862ae7296886da0688d698083
Ryan Quinn <ryan@mazondo.com>
2018-01-29 01:03:07 -0600

    adde current slide support

--- addon/components/siema-container/component.js
+++ addon/components/siema-container/component.js
@@ -2,7 +2,6 @@ import Component from '@ember/component';
 import layout from './template';
 import Siema from 'siema';
 import { assign }  from '@ember/polyfills';
-import { scheduleOnce }  from '@ember/runloop';
 
 export default Component.extend({
   layout,
@@ -10,6 +9,15 @@ export default Component.extend({
   options: null,
   prevSlideCount: 1,
   nextSlideCount: 1,
+  currentSlide: null,
+  hasRendered: null,
+  initialSlide: 0,
+  autoInterval: null,
+
+  init() {
+    this._super(...arguments);
+    this.set('hasRendered', [this.get('initialSlide')]);
+  },
 
   actions: {
     prev() {
@@ -25,18 +33,36 @@ export default Component.extend({
   },
 
   didInsertElement() {
-    scheduleOnce('afterRender', this, () => {
+      let that = this;
       let slidesEl = document.getElementById(`slides-${this.elementId}`);
-      let options = assign({}, this.get('options'), {selector: slidesEl});
+      let options = assign({}, this.get('options'), {
+        selector: slidesEl,
+        startIndex: this.get('initialSlide'),
+        onChange: function() {
+          let current = this.currentSlide;
+          that.get('hasRendered').pushObject(current);
+          that.set('currentSlide', current);
+        }
+      });
       let siema = new Siema(options);
       this.set('_siema', siema);
-    });
+      if (this.get('autoInterval')) {
+        let interval = setInterval(() => {
+          siema.next(1);
+          console.log('next!');
+        }, this.get('autoInterval'));
+        this.set('interval', interval);
+      }
   },
 
   willDestroyElement() {
     let siema = this.get('_siema');
+    let interval = this.get('interval');
     if (siema) {
       siema.destroy();
+    }
+    if (interval) {
+      clearInterval(interval);
     }
   }
 });
